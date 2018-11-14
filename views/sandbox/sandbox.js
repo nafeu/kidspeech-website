79f09b1f5759b441f52c6910661e0e9694c25ccb
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-11-14 11:59:38 -0500

    Implement speech hearing checklist

--- views/sandbox/sandbox.js
+++ views/sandbox/sandbox.js
@@ -11,16 +11,15 @@ angular.module('myApp.sandbox', ['ngRoute'])
 
 .controller('SandboxCtrl', ['$scope', '$document', function($scope, $document) {
 
-  $scope.ages = ["2", "2-4", "4", "6", "8", "10", "12", "14", "16", "16-18", "18", "20", "22", "28", "32", "34", "36", "48", "60"];
-  $scope.expressions = ["Coos", "Laughs", "Babbles", "Mama/Dada", "1st Word", "4-6 Single Words", "Tells Needs", "2-Word Sentences", "50+ Single Words", "Uses Nouns & Verbs", "Talks in Sentences", "Uses All Speech Sounds"];
+  $scope.ages = [0, 2, 3, 4, 6, 8, 10, 12, 14, 16, 17, 18, 20, 22, 28, 32, 34, 36, 48, 60];
   $scope.currentAge;
-  $scope.currentExpression;
+  $scope.currentSection;
 
-  $scope.selectCell = function(index) {
+  $scope.selectAge = function(index) {
     $scope.currentAge = $scope.ages[index];
-    $scope.currentExpression = $scope.expressions[index];
   }
 
-  $scope.selectCell(0);
+  $scope.selectAge(0);
+  $scope.currentSection = 0;
 
 }]);
